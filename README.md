cf4392e7a3e58b37afd07c04a6d1a1f2defcc01a
Randall <takmo@bitwisehero.com>
2018-02-10 16:14:56 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Kattis Quickstart
+
+Just a handy little script to quickly setup a problem environment for Kattis.
+
+You probably don't need to clone this repository, just see download init.sh.
+
+`./init.sh [problem_name: required] [number of sample files: default 1]`
+
+This will automatically setup a folder named after the problem with a sample
+C++ file and a makefile configured to test against all of the samples.
---
+++ init.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+ScriptName=`basename "$0"`
+
+ArgCount=$#
+SampleCount=1
+
+if [ $ArgCount == 0 ]
+then
+    echo "Must provide problem name, and optionally the number of sample files."
+    exit -1
+fi
+
+ProblemName=$1
+
+if [ $ProblemName == $ScriptName ]
+then
+    echo "You cannot name your problem after this script."
+    exit -1
+fi
+
+if [ $ArgCount == 2 ]
+then
+    ArgCount=$2
+fi
+
+mkdir $ProblemName
+
+cat > ${ProblemName}/${ProblemName}.cpp << EOF
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    int testcases = 1;
+    std::cin >> testcases;
+
+    while (testcases--)
+    {
+        std::cout << "Hello, Kattis." << std::endl;
+    }
+
+    return 0;
+}
+EOF
+
+cat > ${ProblemName}/Makefile << EOF
+# auto-generated makefile plz don't edit
+
+all: ${ProblemName} test
+
+clean:
+	rm -f hello
+
+${ProblemName}: ${ProblemName}.cpp
+	g++ -Wall -g --std=c++11 ${ProblemName}.cpp -o ${ProblemName}
+EOF
+
+AllFiles=""
+AllSamples=""
+
+for ((Sample=1; Sample <= $ArgCount; Sample++))
+do
+	SampleName="sample${Sample}"
+	LocalFile="${SampleName}.txt"
+    LongFile="${ProblemName}/${LocalFile}"
+    touch ${LongFile}
+    vim ${LongFile}
+
+	AllFiles="${AllFiles} ${LocalFile}"
+	AllSamples="${AllSamples} ${SampleName}"
+
+	cat >> ${ProblemName}/Makefile << EOF
+
+sample${Sample}: ${ProblemName} ${LocalFile}
+	@echo ---------------------------
+	@echo " ${LocalFile}"
+	@echo ---------------------------
+	@./${ProblemName} < ${LocalFile}
+	@echo
+EOF
+done
+
+cat >> ${ProblemName}/Makefile << EOF
+
+test: ${ProblemName}${AllSamples}
+	@echo ---------------------------
+	@echo " All Tests Finished"
+	@echo ---------------------------
+
+.PHONY: all test${AllSamples}
+
+EOF
+
+echo "Initialized problem ${ProblemName}..."
