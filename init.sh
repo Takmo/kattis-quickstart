40a92a8dc42c43c759470f0f6ad51931fc55b354
Randall <takmo@bitwisehero.com>
2018-03-07 15:56:23 -0600

    Script now downloads sample files automatically.

--- init.sh
+++ init.sh
@@ -1,31 +1,108 @@
 #!/bin/bash
 
-ScriptName=`basename "$0"`
+KattisEnvironment="https://tamu.kattis.com/"
 
+# we need at least one argument (problem name) but can also take the number of sample files
 ArgCount=$#
-SampleCount=1
-
 if [ $ArgCount == 0 ]
 then
 	echo "Must provide problem name, and optionally the number of sample files."
 	exit -1
 fi
 
+# make sure not to name the problem after the script - bad things might happen
 ProblemName=$1
-
+ScriptName=`basename "$0"`
 if [ $ProblemName == $ScriptName ]
 then
 	echo "You cannot name your problem after this script."
 	exit -1
 fi
 
+# if we need to paste in samples, do that
 if [ $ArgCount == 2 ]
 then
-	ArgCount=$2
+	mkdir $ProblemName
+	SampleCount=$2
+	for ((Sample=1; Sample <= $SampleCount; Sample++))
+	do
+		SampleName="sample${Sample}"
+
+		LocalResultFile="${ProblemName}_${SampleName}.ans"
+		LocalSampleFile="${ProblemName}_${SampleName}.in"
+
+		LongResultFile="${ProblemName}/${LocalResultFile}"
+		LongSampleFile="${ProblemName}/${LocalSampleFile}"
+
+		touch ${LongResultFile}
+		touch ${LongSampleFile}
+
+		vim ${LongSampleFile}
+		vim ${LongResultFile}
+	done
 fi
 
-mkdir $ProblemName
+# otherwise, download them ourselves
+if [ $ArgCount == 1 ]
+then
+	SamplesUrl="${KattisEnvironment}problems/${ProblemName}/file/statement/samples.zip"
+	wget ${SamplesUrl}
+	unzip "samples.zip" -d ${ProblemName}
+	rm "samples.zip"
+fi
 
+# get the list of all files in the directory, for the sake of generating tests in Makefile
+InputFiles=(${ProblemName}/*.in)
+
+# generate the initial Makefile without tests
+cat > ${ProblemName}/Makefile << EOF
+# auto-generated makefile plz don't edit
+
+all: ${ProblemName} test
+
+clean:
+	rm -f ${ProblemName}
+
+${ProblemName}: ${ProblemName}.cpp
+	g++ -Wall -g --std=c++11 ${ProblemName}.cpp -o ${ProblemName}
+EOF
+
+# generate the testcases for each test input file
+AllFiles=""
+AllSamples=""
+for LongInputFile in "${InputFiles[@]}"; do
+	InputFile=`basename ${LongInputFile}`
+	OutputFile=`echo ${InputFile} | sed "s/\.in/\.ans/g"`
+	SampleName=`echo ${InputFile} | sed "s/\.in//g"`
+
+	AllFiles="${AllFiles} ${InputFile}"
+	AllSamples="${AllSamples} ${SampleName}"
+
+	cat >> ${ProblemName}/Makefile << EOF
+
+${SampleName}: ${ProblemName}
+	@echo -----------------------------------------------
+	@echo " ${InputFile} (actual, expected)"
+	@echo -----------------------------------------------
+	@./${ProblemName} < ${InputFile}
+	@echo ---
+	@cat ${OutputFile}
+EOF
+done
+
+# generate the final generator for running all tests
+cat >> ${ProblemName}/Makefile << EOF
+
+test: ${ProblemName}${AllSamples}
+	@echo -----------------------------------------------
+	@echo " All Tests Finished"
+	@echo -----------------------------------------------
+
+.PHONY: all test${AllSamples}
+
+EOF
+
+# put a friendly little starter file in the directory :)
 cat > ${ProblemName}/${ProblemName}.cpp << EOF
 #include <iostream>
 
@@ -43,61 +120,5 @@ int main(int argc, char **argv)
 }
 EOF
 
-cat > ${ProblemName}/Makefile << EOF
-# auto-generated makefile plz don't edit
-
-all: ${ProblemName} test
-
-clean:
-	rm -f ${ProblemName}
-
-${ProblemName}: ${ProblemName}.cpp
-	g++ -Wall -g --std=c++11 ${ProblemName}.cpp -o ${ProblemName}
-EOF
-
-AllFiles=""
-AllSamples=""
-
-for ((Sample=1; Sample <= $ArgCount; Sample++))
-do
-	SampleName="sample${Sample}"
-
-	LocalResultFile="${SampleName}.out"
-	LocalSampleFile="${SampleName}.txt"
-
-	LongResultFile="${ProblemName}/${LocalResultFile}"
-	LongSampleFile="${ProblemName}/${LocalSampleFile}"
-
-	touch ${LongResultFile}
-	touch ${LongSampleFile}
-
-	vim ${LongSampleFile}
-	vim ${LongResultFile}
-
-	AllFiles="${AllFiles} ${LocalSampleFile}"
-	AllSamples="${AllSamples} ${SampleName}"
-
-	cat >> ${ProblemName}/Makefile << EOF
-
-sample${Sample}: ${ProblemName}
-	@echo -----------------------------------------------
-	@echo " ${LocalSampleFile} (actual, expected)"
-	@echo -----------------------------------------------
-	@./${ProblemName} < ${LocalSampleFile}
-	@echo ---
-	@cat ${LocalResultFile}
-EOF
-done
-
-cat >> ${ProblemName}/Makefile << EOF
-
-test: ${ProblemName}${AllSamples}
-	@echo -----------------------------------------------
-	@echo " All Tests Finished"
-	@echo -----------------------------------------------
-
-.PHONY: all test${AllSamples}
-
-EOF
-
+# all done!
 echo "Initialized problem ${ProblemName}..."
